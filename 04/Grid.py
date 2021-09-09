17d6adcbc8edbbc7a1684312cc14b8213b3de2d2
sadending82 <sadending82@gmail.com>
2021-09-09 20:07:27 +0900

    Grid.py 추가
    2017180008 김혁동

---
+++ 04/Grid.py
@@ -0,0 +1,25 @@
+import turtle
+
+turtle.shape('turtle')
+
+Posx = 0
+Posy = 0
+count = 5
+
+while(count >= 0):
+    turtle.penup()
+    turtle.goto(Posx, Posy + (count * 100))
+    turtle.pendown()
+    turtle.goto(Posx + 500, Posy + (count * 100))
+    count -= 1
+
+count = 5
+
+while(count >= 0):
+    turtle.penup()
+    turtle.goto(Posx + (count * 100), Posy + 500)
+    turtle.pendown()
+    turtle.goto(Posx + (count * 100), Posy)
+    count -= 1
+
+
