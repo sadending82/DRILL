6217180994a4b6f659c0a66e1960f9dabb05f2b8
sadending82 <sadending82@gmail.com>
2021-11-02 17:21:42 +0900

    11주 내용

---
+++ 11/star.py
@@ -0,0 +1,16 @@
+class Player:
+    type = 'Player'
+
+    def __init__(self):
+        self.x = 100
+
+    def where(self):
+        print(self.x)
+
+player = Player()
+player.where()
+
+print(player.type)
+
+# Player.where()
+Player.where(player)
