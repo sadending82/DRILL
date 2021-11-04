036a1c3e721f72e54f932062270a71c70d3aae56
sadending82 <sadending82@gmail.com>
2021-11-04 15:06:15 +0900

    Update boy.py

--- 11/boy.py
+++ 11/boy.py
@@ -1,7 +1,7 @@
 from pico2d import *
 
 # Boy Event
-RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP = range(4)
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, SLEEP_TIMER = range(5)
 
 # fill here
 
@@ -18,11 +18,11 @@ key_event_table = {
 class IdleState:
     def enter(boy, event):
         if event == RIGHT_DOWN:
-            boy.velocitiy += 1
+            boy.velocity += 1
         elif event == LEFT_DOWN:
             boy.velocity -= 1
         elif event == RIGHT_UP:
-            boy.velocity = -1
+            boy.velocity -= 1
         elif event == LEFT_UP:
             boy.velocity += 1
         boy.timer = 1000
@@ -33,6 +33,8 @@ class IdleState:
     def do(boy):
         boy.frame = (boy.frame + 1) % 8
         boy.timer -= 1
+        if boy.timer == 0:
+            boy.add_event(SLEEP_TIMER)
 
     def draw(boy):
         if boy.dir == 1:
@@ -62,6 +64,7 @@ class RunState:
         boy.x += boy.velocity
         boy.x = clamp(25, boy.x, 800 - 25)
 
+
     def draw(boy):
         if boy.velocity == 1:
             boy.image.clip_draw(boy.frame * 100, 100, 100, 100, boy.x, boy.y)
@@ -70,13 +73,33 @@ class RunState:
 
 
 
-# fill here
+class SleepState:
+
+    def enter(boy, event):
+        boy.frame = 0
+
+    def exit(boy, event):
+        pass
+
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+
+    def draw(boy):
+        if boy.dir == 1:
+            boy.image.clip_composite_draw(boy.frame * 100, 300, 100, 100,
+                                          3.141592 / 2, '', boy.x - 25,boy.y - 25, 100, 100)
+        else:
+            boy.image.clip_composite_draw(boy.frame * 100, 200, 100, 100,
+                                          -3.141592 / 2, '', boy.x + 25, boy.y - 25, 100, 100)
 
 next_state_table = {
     IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState,
-                RIGHT_DOWN: RunState, LEFT_DOWN: RunState},
+                RIGHT_DOWN: RunState, LEFT_DOWN: RunState,
+                SLEEP_TIMER: SleepState},
     RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState,
-               LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState}
+               LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState},
+    SleepState: {LEFT_DOWN: RunState, RIGHT_DOWN: RunState,
+                 LEFT_UP: RunState, RIGHT_UP: RunState}
 }
 
 
@@ -90,9 +113,13 @@ class Boy:
     def __init__(self):
         self.x, self.y = 800 // 2, 90
         self.image = load_image('animation_sheet.png')
-        self.dir = 1
+        self.dir = 0
         self.velocity = 0
+        self.frame = 0
+        self.timer = 0
+
         self.event_que = []
+
         self.cur_state = IdleState
         self.cur_state.enter(self, None)
         pass
@@ -108,16 +135,19 @@ class Boy:
 
 
     def update(self):
+
         self.cur_state.do(self)
+
         if len(self.event_que) > 0:
             event = self.event_que.pop()
             self.cur_state.exit(self,event)
             self.cur_state = next_state_table[self.cur_state][event]
-            self.cur_state.enter(self.event)
+            self.cur_state.enter(self, event)
 
 
     def draw(self):
         self.cur_state.draw(self)
+        debug_print('velocity : ' + str(self.velocity))
         pass
 
 
